8d6c43a961cf1fa1e7c05b4375447b811e12b294
nilpointerinception <nilpointerinception@gmail.com>
2014-08-05 00:02:33 -0400

    lets you pick your own word

--- src/com/recursion/practice/Combinations.java
+++ src/com/recursion/practice/Combinations.java
@@ -1,26 +1,32 @@
-package com.recursion.practice;
+package com.timur.recursion;
 
-public class Combinations{
-	
-	public static void main(String[] args){
-		combination("ABC");
+import java.util.Scanner;
+
+public class Combinations {
+
+	public static void main(String[] args) {
+		System.out.println("Please enter something");
+		Scanner scan = new Scanner(System.in);
+		combination(scan.nextLine());
+		scan.close();
 	}
-	
-	public static void combination(String word){
+
+	public static void combination(String word) {
 		printCombination("", word);
 	}
-	
-	public static void printCombination(String prefix, String word){
-		if(word.length() <= 1){
+
+	public static void printCombination(String prefix, String word) {
+		if (word.length() <= 1) {
 			System.out.println(prefix + word);
-		}else{
+		} else {
 			printCombination(prefix + word.charAt(0), word.substring(1));
-			
-			for(int i = 1; i <= word.length() - 2; i++){
-				printCombination(prefix + word.charAt(i), word.substring(0, i) + word.substring(i + 1));
+
+			for (int i = 1; i <= word.length() - 2; i++) {
+				printCombination(prefix + word.charAt(i), word.substring(0, i)
+						+ word.substring(i + 1));
 			}
-			printCombination(prefix + word.charAt(word.length() - 1), word.substring(0, word.length() - 1));
+			printCombination(prefix + word.charAt(word.length() - 1),
+					word.substring(0, word.length() - 1));
 		}
 	}
-	
 }
